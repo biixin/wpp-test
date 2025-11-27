5779e5b8fa84beaa1e77e0b5058c2ea499fd1651
biixin <113357477+biixin@users.noreply.github.com>
2025-11-27 15:59:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const PORT = process.env.PORT || 3000;
 
 // Lista de números
 const numbers = [
-
+  "13439007613",
+  "18739007935",
+  "18737777695",
+  "12494797753"
 ];
 
 // Lista de mensagens
